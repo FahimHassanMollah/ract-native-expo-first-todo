8793f073444328408143d0c836af67c33b10118e
fahimhassanmollah <fahimhassan0197@gmail.com>
2025-12-09 16:05:27 +0600

    feat: add Convex schema and todos functionality
    - Created a new schema for todos with fields: text and isCompleted.
    - Implemented CRUD operations for todos including add, toggle, delete, update, and clear all todos.
    - Added TypeScript configuration for Convex functions.
    - Updated package dependencies to include Convex and related libraries.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,66 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import { LinearGradient } from 'expo-linear-gradient';
 
 const index = () => {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+  const todos = useQuery(api.todos.getTodos);
+  console.log(todos);
+  const addTodo = useMutation(api.todos.addTodo);
+  const clearAllTodos = useMutation(api.todos.clearAllTodos);
+  
+  const homeStyles = createHomeStyles(colors);
+  const style = StyleSheet.create({
+    title : {
+      fontSize: 24,
+      fontWeight: "bold",
+      color: colors.text,
+    },
+    button: {
+      marginTop: 20,
+      padding: 10,
+      backgroundColor: colors.bg,
+    },
+  });
+
   return (
-    <SafeAreaView>
+    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}  >
+       <SafeAreaView style={homeStyles.safeArea}>
       <View>
-        <Text>index</Text>
+        <Text>indexs</Text>
         <Pressable
-         style={{ marginTop: 20, padding: 10, backgroundColor: "lightgray" }}
+         style={style.button}
           onPress={() => {
             toggleDarkMode();
           }}
         >
-          <Text>Toggle Theme</Text>
+          <Text style={style.title}>Toggle Theme</Text>
+        </Pressable>
+        <Pressable
+         style={{ marginTop: 20, padding: 10, backgroundColor: "lightgray" }}
+          onPress={async () => {
+            await addTodo({ text: "New Todo Item" });
+          }}
+        >
+          <Text>Add Todo</Text>
+        </Pressable>
+        <Pressable
+         style={{ marginTop: 20, padding: 10, backgroundColor: "lightgray" }}
+          onPress={async () => {
+            await clearAllTodos();
+          }}
+        >
+          <Text>Clear All Todos</Text>
         </Pressable>
       </View>
     </SafeAreaView>
+    </LinearGradient>
+   
   );
 };
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,18 @@
 import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 
 export default function RootLayout() {
+  const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+    unsavedChangesWarning: false,
+  });
   return (
-    <ThemeProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
+    <ConvexProvider client={convex}>
+      <ThemeProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </ConvexProvider>
   );
 }
